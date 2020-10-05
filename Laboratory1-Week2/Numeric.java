da63d1110e57efd9fcd7aef4bc5f08bcbf4013cf
Rozerin Baran <63159237+RozerinB@users.noreply.github.com>
2020-10-05 16:38:32 +0100

    Update Numeric.java

--- Laboratory1-Week2/Numeric.java
+++ Laboratory1-Week2/Numeric.java
@@ -1,22 +1,27 @@
+package Lab1;
+
 public class Numeric {
-    public static void main (String[] args){
-        /**
-      Tests whether two floating-point numbers are
-      equal, except for a roundoff error
-      x a floating-point number
-      y a floating-point number
-      returns true if x and y are approximately equal
-   */
+	/**
+    Tests whether two floating-point numbers are
+    equal, except for a roundoff error
+    x a floating-point number
+    y a floating-point number
+    returns true if x and y are approximately equal
+ */
+
+	public static void main (String args[]) {
+		approxEqual(5.5, 5.6);
+	}
+	
+ public static boolean approxEqual(double x, double y)
+ {  
+    final double EPSILON = 1E-10;
+    if (Math.abs(x-y)<EPSILON)
+    {
+  	  return(true);
     }
-    
-   public static boolean approxEqual(double x, double y)
-   {  
-      final double EPSILON = 1E-10;
-      if (Math.abs(x-y)<EPSILON)
-      {
-    	  return(true);
-      }
-      return(false);
-   }
-   // more numeric methods can be added here
+    return(false);
+ }
+ // more numeric methods can be added here
+
 }
