58ba863f507fbfa0147ec1e67d9d28b07ca26163
RozerinB <rozbaran@icloud.com>
2020-10-12 00:46:19 +0100

    Linear Summation

---
+++ Laboratory1-Week2/linearSummation.java
@@ -0,0 +1,30 @@
+package Lab2;
+public class linearSummation {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis(); //Get start line of program
+		int n = 15;
+		LinearSum(n);
+		long end = System.currentTimeMillis(); //End time of program
+		long runTime= end - start; //calculate duration
+		System.out.println(runTime);
+	}
+
+	public static void LinearSum(int n) {
+
+		int sum = 0;
+		
+		for (int i = 1; i<=n; i++) { 
+			sum = sum + i ;
+		}
+		//sum = sum + (n-1) + 1; 
+		System.out.println(sum);
+	}
+	/*
+	 * Able to remove linear 15 by using i<= n
+	 * However to use Linear Summation as per Linear Equation
+	 * We should use sum = sum + (num-1) + 1
+	 */
+	
+}
+
